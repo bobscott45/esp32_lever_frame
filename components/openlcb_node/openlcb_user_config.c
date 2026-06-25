4caa5e2244db9c4b7c80b079c6437cb2be92f616
robert <bob@rwscott.co.uk>
2026-06-25 12:04:04 +0100

    chore: bump OpenLCB node versions to 1.6.0 to pass pre-release check

--- components/openlcb_node/openlcb_user_config.c
+++ components/openlcb_node/openlcb_user_config.c
@@ -69,8 +69,8 @@ const node_parameters_t openlcb_user_config_node_parameters = {
     .snip.mfg_version = 4,
     .snip.name = "Robert Scott",
     .snip.model = "ESP32 LCC Lever Frame",
-    .snip.hardware_version = "1.4.0",
-    .snip.software_version = "1.4.0",
+    .snip.hardware_version = "1.6.0",
+    .snip.software_version = "1.6.0",
     .snip.user_version = 2,
 
 
